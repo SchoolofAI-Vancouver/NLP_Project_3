478d3546115f4bffacdbb34dbbff4689eeb42c00
Samson <xbhuang93@hotmail.com>
2018-12-03 11:59:49 -0800

    Using logger to track training progress & style fix

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+import gc
+import os
+import pickle
+import warnings
+
 import numpy as np
 import pandas as pd
 
@@ -10,14 +15,12 @@ from keras.models import Model
 from keras.layers import Input, Dense, Embedding, SpatialDropout1D, concatenate
 from keras.layers import GRU, Bidirectional, GlobalAveragePooling1D, GlobalMaxPooling1D
 from keras.preprocessing import text, sequence
-from keras.callbacks import Callback
+from keras.callbacks import TensorBoard
 
-import warnings
+from utils import get_logger, get_root
+
+
 warnings.filterwarnings('ignore')
-
-import gc
-import os
-import pickle
 os.environ['OMP_NUM_THREADS'] = '4'
 
 
@@ -44,9 +47,12 @@ class Preprocess(object):
 
 
 def get_embeddings(embed_file, word_index, max_features, embed_size):
-    def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
+
+    def get_coefs(word, *arr):
+        return word, np.asarray(arr, dtype='float32')
+
     embeddings_pretrained = dict(get_coefs(*o.rstrip().rsplit(' ')) for o in open(embed_file, encoding="utf8", errors='ignore'))
- 
+
     nb_words = min(max_features, len(word_index))
     embedding_matrix = np.zeros((nb_words, embed_size))
     for word, i in word_index.items():
@@ -55,19 +61,21 @@ def get_embeddings(embed_file, word_index, max_features, embed_size):
         embedding_vector = embeddings_pretrained.get(word)
         if embedding_vector is not None:
             embedding_matrix[i] = embedding_vector
-    
+
     return embedding_matrix
 
 
-class RocAucEvaluation(Callback):
+class RocAucEvaluation(TensorBoard):
 
     def __init__(self, validation_data=(), interval=1):
-        super(Callback, self).__init__()
+        super().__init__()
 
         self.X_val, self.y_val = validation_data
         self.interval = interval
 
-    def on_epoch_end(self, epoch, logs={}):
+    def on_epoch_end(self, epoch, logs=None):
+        super().on_epoch_end()
+
         if epoch % self.interval == 0:
             y_pred = self.model.predict(self.X_val, verbose=0)
             score = roc_auc_score(self.y_val, y_pred)
@@ -94,42 +102,57 @@ def get_model(maxlen, max_features, embed_size, embedding_matrix):
 
 if __name__ == "__main__":
 
-    EMBEDDING_FILE = '../assets/embedding/fasttext-crawl-300d-2m/crawl-300d-2M.vec'
-    classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+    DIR_ROOT = get_root()
+    DIR_ASSETS = os.path.join(DIR_ROOT, 'assets')
+    MODEL_PATH = os.path.join(DIR_ASSETS, 'model')
+    LOG_PATH = os.path.join(DIR_ASSETS, 'tb_logs')
+    EMBEDDING_FILE = os.path.join(DIR_ASSETS, 'embedding', 'fasttext-crawl-300d-2m', 'crawl-300d-2M.vec')
+    DATA_FILE = os.path.join(DIR_ASSETS, 'data', 'train.csv')
 
     MAX_FEATURES = 30000
     MAXLEN = 100
     EMBED_SIZE = 300
+    TRAIN_SIZE = 0.95
     BATCH_SIZE = 32
     EPOCHS = 2
     np.random.seed(42)
 
-    print("Load data..")
-    train = pd.read_csv('../assets/data/train.csv')
-    
+    logger = get_logger()
+
+    logger.info(f"Loading data: {DATA_FILE}")
+    train = pd.read_csv(DATA_FILE)
+    classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+
     features = train["comment_text"].fillna("# #").values
     target = convert_binary_toxic(train, classes)
     del train
     gc.collect()
-    
-    print("Transform data..")
+
+    logger.info(f"Transforming data")
     preprocessor = Preprocess(max_features=MAX_FEATURES, maxlen=MAXLEN)
     preprocessor.fit_texts(list(features))
     features = preprocessor.transform_texts(features)
 
-    print("Load embedding..")
+    PRERPOCESSOR_FILE = os.path.join(MODEL_PATH, 'preprocessor.pkl')
+    logger.info(f"Saving the text transformer: {PRERPOCESSOR_FILE}")
+    with open(PRERPOCESSOR_FILE, 'wb') as file:
+        pickle.dump(preprocessor, file)
+    del preprocessor
+    gc.collect()
+
+    logger.info(f"Loading embedding vectors: {EMBEDDING_FILE}")
     word_index = preprocessor.tokenizer.word_index
     embedding_matrix = get_embeddings(EMBEDDING_FILE, word_index, MAX_FEATURES, EMBED_SIZE)
 
-    print("Train model..")
-    model = get_model()
+    logger.info(f"Model training, train size: {TRAIN_SIZE}")
+    X_train, X_val, y_train, y_val = train_test_split(features, target, train_size=TRAIN_SIZE, random_state=233)
+    RocAuc = RocAucEvaluation(log_dir=LOG_PATH, batch_size=BATCH_SIZE, validation_data=(X_val, y_val), interval=1)
 
-    X_train, X_val, y_train, y_val = train_test_split(features, target, train_size=0.95, random_state=233)
-    RocAuc = RocAucEvaluation(validation_data=(X_val, y_val), interval=1)
+    model = get_model(MAXLEN, MAX_FEATURES, EMBED_SIZE, embedding_matrix)
 
-    hist = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_val, y_val),
-                    callbacks=[RocAuc], verbose=1)
+    hist = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
+                     validation_data=(X_val, y_val), callbacks=[RocAuc], verbose=1)
 
-    model.save('../assets/model/model.h5')
-    with open('preprocessor.pkl', 'wb') as file:
-        pickle.dump(preprocessor, file)
+    MODEL_FILE = os.path.join(MODEL_PATH, 'model.h5')
+    logger.info(f"Saving the model: {MODEL_FILE}")
+    model.save(MODEL_PATH)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,25 @@
 #! /usr/bin/env python
 
 from pathlib import Path
+import logging
 
 
 def get_root():
     """Return project root folder"""
     return Path(__file__).parent.parent
+
+
+def get_logger(level=logging.INFO):
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.info("Logging successfully configured!")
+
+    return logger
