ac89a4a6c1f4dc0d22b1de3a7b78947123cac12d
Samson <xbhuang93@hotmail.com>
2018-12-02 23:38:17 -0800

    Some refactor or code on model.py

--- src/model.py
+++ src/model.py
@@ -26,34 +26,62 @@ train = pd.read_csv('../assets/data/train.csv')
 
 classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
+
+def convert_binary_toxic(data, classes):
+    target = data[classes].values != np.zeros((len(data), 6))
+    binary = target.any(axis=1)
+    return binary
+
+
+target = convert_binary_toxic(train, classes)
 features = train["comment_text"].fillna("# #").values
-target = train[classes].values != np.zeros((len(train), 6))
-target = target.any(axis=1)
 del train
 gc.collect()
 
-max_features = 30000
-maxlen = 100
-embed_size = 300
-
-tokenizer = text.Tokenizer(num_words=max_features)
-tokenizer.fit_on_texts(list(features))
-features = tokenizer.texts_to_sequences(features)
-features = sequence.pad_sequences(features, maxlen=maxlen)
+MAX_FEATURES = 30000
+MAXLEN = 100
+EMBED_SIZE = 300
 
 
-def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
-embeddings_index = dict(get_coefs(*o.rstrip().rsplit(' ')) for o in open(EMBEDDING_FILE, encoding="utf8", errors='ignore'))
+class Preprocess(object):
 
-word_index = tokenizer.word_index
-nb_words = min(max_features, len(word_index))
-embedding_matrix = np.zeros((nb_words, embed_size))
-for word, i in word_index.items():
-    if i >= max_features:
-        continue
-    embedding_vector = embeddings_index.get(word)
-    if embedding_vector is not None: 
-        embedding_matrix[i] = embedding_vector
+    def __init__(self, max_features, maxlen):
+        self.max_features = max_features
+        self.maxlen = maxlen
+
+    def fit_texts(self, list_sentences):
+        self.tokenizer = text.Tokenizer(num_words=self.max_features)
+        self.tokenizer.fit_on_texts(list_sentences)
+
+    def transform_texts(self, list_sentences):
+        tokenized_sentences = self.tokenizer.texts_to_sequences(list_sentences)
+        features = sequence.pad_sequences(tokenized_sentences, maxlen=self.maxlen)
+        return features
+
+
+pre = Preprocess(max_features=MAX_FEATURES, maxlen=MAXLEN)
+pre.fit_texts(list(features))
+features = pre.transform_texts(features)
+
+
+def get_embeddings(embed_file, word_index, max_features, embed_size):
+    def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_pretrained = dict(get_coefs(*o.rstrip().rsplit(' ')) for o in open(embed_file, encoding="utf8", errors='ignore'))
+ 
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.zeros((nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features:
+            continue
+        embedding_vector = embeddings_pretrained.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+    
+    return embedding_matrix
+
+
+word_index = pre.tokenizer.word_index
+embedding_matrix = get_embeddings(EMBEDDING_FILE, word_index, MAX_FEATURES, EMBED_SIZE)
 
 
 class RocAucEvaluation(Callback):
@@ -71,17 +99,17 @@ class RocAucEvaluation(Callback):
             print("\n ROC-AUC - epoch: %d - score: %.6f \n" % (epoch+1, score))
 
 
-def get_model():
-    inp = Input(shape=(maxlen, ))
-    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+def get_model(maxlen, max_features, embed_size, embedding_matrix):
+    input = Input(shape=(maxlen, ))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(input)
     x = SpatialDropout1D(0.2)(x)
     x = Bidirectional(GRU(80, return_sequences=True))(x)
     avg_pool = GlobalAveragePooling1D()(x)
     max_pool = GlobalMaxPooling1D()(x)
     conc = concatenate([avg_pool, max_pool])
-    outp = Dense(1, activation="sigmoid")(conc)
+    output = Dense(1, activation="sigmoid")(conc)
 
-    model = Model(inputs=inp, outputs=outp)
+    model = Model(inputs=input, outputs=output)
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
@@ -93,13 +121,13 @@ if __name__ == "__main__":
 
     model = get_model()
 
-    batch_size = 32
-    epochs = 2
+    BATCH_SIZE = 32
+    EPOCHS = 2
 
     X_train, X_val, y_train, y_val = train_test_split(features, target, train_size=0.95, random_state=233)
     RocAuc = RocAucEvaluation(validation_data=(X_val, y_val), interval=1)
 
-    hist = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val),
-                    callbacks=[RocAuc], verbose=2)
+    hist = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_val, y_val),
+                    callbacks=[RocAuc], verbose=1)
 
     model.save('../assets/model/model.h5')
